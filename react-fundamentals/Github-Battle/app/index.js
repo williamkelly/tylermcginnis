797156a81d22d216a3bd031297d404ed82f37e3e
William Kelly <william.kelly@blueyonder.co.uk>
2018-12-22 17:43:03 +0000

    save.

--- react-fundamentals/Github-Battle/app/index.js
+++ react-fundamentals/Github-Battle/app/index.js
@@ -3,6 +3,6 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 require('./index.css');
 
-import App from './components/App'
+import App from './components/App';
 
 ReactDOM.render(<App />, document.getElementById('app'));
