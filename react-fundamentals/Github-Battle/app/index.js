dc94a446cfc187ca42fdb8a7b0e14c515f348c1e
William Kelly <william.kelly@blueyonder.co.uk>
2018-12-22 17:41:39 +0000

    tyler mcginnis react fundamentals

--- react-fundamentals/Github-Battle/app/index.js
+++ react-fundamentals/Github-Battle/app/index.js
@@ -3,14 +3,6 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 require('./index.css');
 
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        Hello World!
-      </div>
-    )
-  }
-}
+import App from './components/App'
 
 ReactDOM.render(<App />, document.getElementById('app'));
