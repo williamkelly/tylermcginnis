759f5bb2f4463dacf82c42392525473a7ed69a42
William Kelly <william.kelly@blueyonder.co.uk>
2018-12-24 16:35:58 +0000

    87% complete, Defaulting props with defaultProps

--- react-fundamentals/Github-Battle/app/components/App.js
+++ react-fundamentals/Github-Battle/app/components/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Popular from './Popular';
 import Home from './Home';
 import Battle from './Battle';
+import Results from './Results';
 import Nav from './Nav';
 
 class App extends Component {
@@ -14,6 +15,7 @@ class App extends Component {
           <Switch>
             <Route exact path='/' component={Home} />
             <Route exact path='/battle' component={Battle} />
+            <Route path='/battle/results' component={Results} />
             <Route path='/popular' component={Popular} />
             <Route render={function () {
               return <p>Not Found</p>
